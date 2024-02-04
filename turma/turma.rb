cac579c8534d900c5ae16a6e12f8918c93e7bb68
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-04 04:02:50 -0300

    add listar dados professor

--- turma/turma.rb
+++ turma/turma.rb
@@ -6,6 +6,8 @@ class Turma
         @professor = professor
     end
 
+    def  dados_turma
+        puts "Matéria: #{professor.disciplina} , Professor(a): #{professor.nome}"
     def listar alunos
         @alunos.each do |aluno|
             puts "Nome: #{aluno.nome}, Matrícula: #{aluno.matricula}"
