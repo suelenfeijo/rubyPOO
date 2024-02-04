e26b65ba6970685b14a17c5223064a8e2dc3b9a8
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-04 04:50:20 -0300

    add métodos listar , sair e adicionar contato

--- projeto_agenda/agenda.rb
+++ projeto_agenda/agenda.rb
@@ -1,4 +1,40 @@
-puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. 
-Editar Contato\n5. Remover Contato\n0. Sair"
+
+#QUANDO A VÁRIAVEL ESTÁ COM O @ pode ser acessado por todo o programa atual
+@agenda = [
+    {nome: "Nívea", telefone: "8199998888"},
+    {nome: "Alexandra", telefone: "81977776666"}
+]
+
+def allContacts
+    @agenda.each do |contato|
+        puts "#{contato[:nome]} - #{contato[:telefone]}"
+        puts "----------------------------------------"
+    end
+end
+
+def addContact
+    print "Nome: "
+    nome = gets.chomp
+    print "Telefone: "
+    telefone = gets.chomp
+
+    @agenda << {nome: nome, telefone: telefone  }
+end
 
 
+loop do
+
+puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+
+    codigo = gets.chomp.to_i
+
+    case 
+    when codigo == 0
+        puts "See you later!"
+        break
+    when codigo == 1
+        allContacts
+    when codigo == 2
+        addContact
+    end
+end
